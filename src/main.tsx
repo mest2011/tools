83be9f3076b7d75950b0fd5e3826bb76afe16bc9
Miguel Eto Sobral Teixeira <mest2011mest@gmail.com>
2024-09-26 20:46:24 -0300

    feat: added routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App.tsx";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme/index.ts";
+import { App } from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </StrictMode>
 );
