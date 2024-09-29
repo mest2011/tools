a9cdb7cd38a2fba812d3ff843b8de05dffb7afb8
Miguel Eto Sobral Teixeira <mest2011mest@gmail.com>
2024-09-28 21:57:25 -0300

    feat: added date tool

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,13 +2,15 @@ import { Route, Routes as RoutesDom } from "react-router-dom";
 import { PasswordGenerator } from "./components/Organisms/PasswordGenerator";
 import { TextTransformer } from "./components/Organisms/TextTransformer";
 import { Ip } from "./components/Organisms/Ip";
+import { DateComponent } from "./components/Organisms/Date";
 
 export const Routes = () => {
   return (
     <RoutesDom>
-      <Route path="/password" element={<PasswordGenerator />}/>
-      <Route path="/text" element={<TextTransformer />}/>
-      <Route path="/ip" element={<Ip />}/>
+      <Route path="/password" element={<PasswordGenerator />} />
+      <Route path="/text" element={<TextTransformer />} />
+      <Route path="/ip" element={<Ip />} />
+      <Route path="/date" element={<DateComponent />} />
     </RoutesDom>
   );
 };
