01a0474138de0ed899ea5d936dd265f8c4f9d0aa
Miguel Eto Sobral Teixeira <mest2011mest@gmail.com>
2024-11-19 22:18:40 -0300

    feat: added constants

---
+++ src/model/constants/calendary.ts
@@ -0,0 +1,24 @@
+export const days = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+  ];
+  
+  export const months = [
+    "Janeiro",
+    "Fevereiro",
+    "Março",
+    "Abril",
+    "Maio",
+    "Junho",
+    "Julho",
+    "Agosto",
+    "Setembro",
+    "Outubro",
+    "Novembro",
+    "Dezembro",
+  ];
